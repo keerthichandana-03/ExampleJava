c5fb2ce4466a1a48e6b39aaf539168ed32271897
chandana <chandana@LAPTOP-DTQQOOD4>
2024-07-13 20:02:26 +0530

    First Commit

---
+++ src/com/study/Conditional.java
@@ -0,0 +1,21 @@
+package com.study;
+
+import java.util.Scanner;
+
+public class Conditional {
+
+	public static void main(String[] args) {
+		
+int i=10;
+if(i==10)
+{
+	System.out.println("Hello");
+	
+}
+else
+{
+	System.out.println("Hey");
+}
+	}
+
+}
